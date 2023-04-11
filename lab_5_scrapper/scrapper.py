b011a99de52ab6873dd15a5ad3b820a26d941cc3
Pavel Shkunov <pvlshkunov@gmail.com>
2023-04-11 16:19:54 +0300

    small fixes

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -181,6 +181,7 @@ def make_request(url: str, config: Config) -> requests.models.Response:
         verify=config.get_verify_certificate()
     )
     response.encoding = config.get_encoding()
+    response.raise_for_status()
     return response
 
 
